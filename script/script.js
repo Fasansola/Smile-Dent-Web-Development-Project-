dbfc53fc16c7a5516ced169d93d64a2d57df0851
Fasan Olusola <justwpstudio@gmail.com>
2024-02-18 10:40:09 +0100

    Home page Fully Developed and Responsive on All Devices

---
+++ script/script.js
@@ -0,0 +1,5 @@
+let avatars = document.querySelectorAll('.hero-thumbnail')
+avatars[0].style.zIndex = '3';
+avatars[1].style.zIndex = '2';
+avatars[2].style.zIndex = '1';
+avatars[3].style.zIndex = '0';
