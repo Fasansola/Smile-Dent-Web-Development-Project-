863cea70c545872fd072474d01a8622722adb73c
Fasan Olusola <justwpstudio@gmail.com>
2024-02-28 11:41:30 +0100

    Mobile menu created and added to all pages

--- script/script.js
+++ script/script.js
@@ -1,5 +1,26 @@
 let avatars = document.querySelectorAll('.hero-thumbnail')
-avatars[0].style.zIndex = '3';
-avatars[1].style.zIndex = '2';
-avatars[2].style.zIndex = '1';
-avatars[3].style.zIndex = '0';
+
+if (avatars.length != 0) {
+    avatars[0].style.zIndex = '3';
+    avatars[1].style.zIndex = '2';
+    avatars[2].style.zIndex = '1';
+    avatars[3].style.zIndex = '0';
+}
+
+
+let openMenu = document.getElementById("open-menu");
+let closeMenu = document.getElementById("close-menu");
+let mobileMenu = document.querySelector(".menu-bg");
+
+
+openMenu.addEventListener('click', showMenu);
+closeMenu.addEventListener('click', hideMenu);
+
+
+function showMenu() {
+    mobileMenu.style.setProperty("visibility", "visible", "important");
+}
+
+function hideMenu() {
+    mobileMenu.style.setProperty("visibility", "hidden", "important");
+}
